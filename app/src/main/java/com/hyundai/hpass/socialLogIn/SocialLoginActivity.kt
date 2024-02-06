dfb1e76497d8fc75cf4fce7413a302adc5844827
= <ptibabmgo@gmail.com>
2024-02-06 19:24:47 +0900

    feat: viewbinding 추가

--- app/src/main/java/com/hyundai/hpass/socialLogIn/SocialLoginActivity.kt
+++ app/src/main/java/com/hyundai/hpass/socialLogIn/SocialLoginActivity.kt
@@ -3,10 +3,14 @@ package com.hyundai.hpass.socialLogIn
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.hyundai.hpass.R
+import com.hyundai.hpass.databinding.ActivitySocialLoginBinding
 
 class SocialLoginActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySocialLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_social_login)
+        binding = ActivitySocialLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
