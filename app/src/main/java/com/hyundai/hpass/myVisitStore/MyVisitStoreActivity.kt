dee09112f899a7e9bba98884acc6bfa482f564a2
kimeunseo58 <alan0221@naver.com>
2024-02-12 04:58:43 +0900

    feat: 오늘의 매장 UI

--- app/src/main/java/com/hyundai/hpass/myVisitStore/MyVisitStoreActivity.kt
+++ app/src/main/java/com/hyundai/hpass/myVisitStore/MyVisitStoreActivity.kt
@@ -3,10 +3,19 @@ package com.hyundai.hpass.myVisitStore
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.hyundai.hpass.R
+import com.hyundai.hpass.databinding.MyVisitStoreActivityMyVisitStoreBinding
 
 class MyVisitStoreActivity : AppCompatActivity() {
+    lateinit var binding: MyVisitStoreActivityMyVisitStoreBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.my_visit_store_activity_my_visit_store)
+        binding = MyVisitStoreActivityMyVisitStoreBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val check_list_item = binding.myVisitStoreCheckListItem
+        check_list_item.adapter = CheckListAdapter()
+
+        val store_list_item = binding.myVisitStoreStoreList
+        store_list_item.adapter = StoreListAdapter()
     }
 }
---
+++ app/src/main/java/com/hyundai/hpass/myVisitStore/model/response/StoreListResponse.kt
@@ -0,0 +1,8 @@
+package com.hyundai.hpass.myVisitStore.model.response
+
+data class StoreListResponse(
+    val floor : String,
+    val img : String,
+    val title : String,
+    val status: Boolean
+)
