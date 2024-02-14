56fbf09c8450e578275d5096715da6f6a0bbdf76
최현서 <117807082+ha01032@users.noreply.github.com>
2024-02-14 17:43:34 +0900

    fix: Api설정 수정

--- app/src/main/java/com/hyundai/hpass/network/RetrofitClient.kt
+++ app/src/main/java/com/hyundai/hpass/network/RetrofitClient.kt
@@ -1,6 +1,11 @@
 package com.hyundai.hpass.network
 
 import com.hyundai.hpass.BuildConfig
+import com.hyundai.hpass.BuildConfig.BASE_URL
+
+import com.hyundai.hpass.socialLogIn.MemberService
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 /**
@@ -10,4 +15,13 @@ import com.hyundai.hpass.BuildConfig
  */
 object RetrofitClient {
     private const val BASE_URL = BuildConfig.BASE_URL
+//    private const val BASE_URL = "http://10.0.2.2:8080/"
+
+    val memberService: MemberService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(MemberService::class.java)
+    }
 }
