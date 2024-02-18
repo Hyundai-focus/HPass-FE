dda594c66c15303f2d1fccb969fda01d638bb2bf
Eunseo Kim <alan0221@naver.com>
2024-02-18 21:45:51 +0900

    feat: 오늘의 상점 api 통신_방문처리 제외 (#12)

---
+++ app/src/main/java/com/hyundai/hpass/myVisitStore/MyVisitStoreService.kt
@@ -0,0 +1,19 @@
+package com.hyundai.hpass.myVisitStore
+
+import com.hyundai.hpass.myVisitStore.model.response.StoreListResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Header
+
+/**
+ *
+ * @author 김은서
+ *
+ */
+
+interface MyVisitStoreService {
+    @GET("/store/visit")
+    suspend fun getStoreList(
+        @Header("Authorization") Authorization: String
+    ): Response<List<StoreListResponse>>
+}
--- app/src/main/java/com/hyundai/hpass/myVisitStore/StoreListAdapter.kt
+++ app/src/main/java/com/hyundai/hpass/myVisitStore/StoreListAdapter.kt
@@ -13,16 +13,8 @@ import com.hyundai.hpass.myVisitStore.model.response.StoreListResponse
  * @author 김은서
  *
  */
-class StoreListAdapter : RecyclerView.Adapter<StoreListAdapter.StoreListViewHolder>() {
+class StoreListAdapter(private val items: List<StoreListResponse>) : RecyclerView.Adapter<StoreListAdapter.StoreListViewHolder>() {
     class StoreListViewHolder(val binding: MyVisitStoreStoreBinding) : RecyclerView.ViewHolder(binding.root)
-    //통신을 통해 받아올 데이터 -> 각 층, 현재 체크 되어있는지 안되어있는지
-    private val items: List<StoreListResponse> = listOf(
-        StoreListResponse("1F Exclusive Label", "https://www.ehyundai.com/images/webhome2/store/img_the_hyundai_seoul_floor_1f.png", "1층매장이름",true),
-        StoreListResponse("2F Modern Mood", "https://www.ehyundai.com/images/webhome2/store/img_the_hyundai_seoul_floor_2f.png", "2층매장이름",true),
-        StoreListResponse("3F About Fashion", "https://www.ehyundai.com/images/webhome2/store/img_the_hyundai_seoul_floor_3f.png", "3층매장이름",false),
-        StoreListResponse("4F Life & Balance", "https://www.ehyundai.com/images/webhome2/store/img_the_hyundai_seoul_floor_4f.png", "4층매장이름",true),
-        StoreListResponse("5F Sounds Forest", "https://www.ehyundai.com/images/webhome2/store/img_the_hyundai_seoul_floor_5f.png", "5층매장이름",false)
-    )
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StoreListViewHolder {
         val binding = MyVisitStoreStoreBinding.inflate(LayoutInflater.from(parent.context),parent, false)
@@ -33,11 +25,11 @@ class StoreListAdapter : RecyclerView.Adapter<StoreListAdapter.StoreListViewHold
         val item = items[position] // 데이터 리스트에서 해당 위치의 데이터를 가져옵니다.
         holder.binding.apply {
             Glide.with(holder.itemView.context)
-                .load(item.img)
+                .load(item.storeImg)
                 .into(myVisitStoreImage)
-            myVisitStoreFloor.text = item.floor
-            myVisitStoreTitle.text = item.title
-            if(item.status) myVisitStoreStoreStatus.visibility = View.VISIBLE
+            myVisitStoreFloor.text = item.storeFloor
+            myVisitStoreTitle.text = item.storeBrand
+            if(item.visitStatus) myVisitStoreStoreStatus.visibility = View.VISIBLE
         }
     }
 
