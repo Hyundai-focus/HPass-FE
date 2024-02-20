2601c653705204e4a3979793b44e35d24607b5f0
최현서 <117807082+ha01032@users.noreply.github.com>
2024-02-20 16:03:57 +0900

    feat: retrofit추가

--- app/src/main/java/com/hyundai/hpass/network/RetrofitClient.kt
+++ app/src/main/java/com/hyundai/hpass/network/RetrofitClient.kt
@@ -9,6 +9,7 @@ import com.hyundai.hpass.myVisitStore.MyVisitStoreService
 import com.hyundai.hpass.newProduct.NewProductService
 import com.hyundai.hpass.popUpStore.PopUpBookingService
 import com.hyundai.hpass.socialLogIn.MemberService
+import com.hyundai.hpass.subscription.SubscriptionService
 
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -21,8 +22,8 @@ import retrofit2.converter.scalars.ScalarsConverterFactory
  *
  */
 object RetrofitClient {
-//    private const val BASE_URL = BuildConfig.BASE_URL
-    private const val BASE_URL = "http://10.0.2.2:8080/"
+    private const val BASE_URL = BuildConfig.BASE_URL
+//    private const val BASE_URL = "http://10.0.2.2:8080/"
 
     val memberService: MemberService by lazy {
         Retrofit.Builder()
@@ -36,6 +37,13 @@ object RetrofitClient {
     val gson = GsonBuilder()
         .setLenient()
         .create()
+    val subscriptionService: SubscriptionService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+            .create(SubscriptionService::class.java)
+    }
 
     val bookingService: PopUpBookingService by lazy {
         Retrofit.Builder()
@@ -44,7 +52,7 @@ object RetrofitClient {
             .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
             .create(PopUpBookingService::class.java)
-
+    }
     val myVisitStoreService: MyVisitStoreService by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
