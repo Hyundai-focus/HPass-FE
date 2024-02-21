bd602c4fe96e86db5a6940f70cbac6f1b4706709
최현서 <117807082+ha01032@users.noreply.github.com>
2024-02-21 10:23:22 +0900

    feat: 메인화면 수정

--- app/src/main/java/com/hyundai/hpass/main/HamburgerMenuActivity.kt
+++ app/src/main/java/com/hyundai/hpass/main/HamburgerMenuActivity.kt
@@ -2,25 +2,33 @@ package com.hyundai.hpass.main
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.hyundai.hpass.BuildConfig
 import com.hyundai.hpass.R
 import com.hyundai.hpass.databinding.HamburgerMenuActivityBinding
 import com.hyundai.hpass.myPage.MyPageMainActivity
 import com.hyundai.hpass.socialLogIn.MyApplication
+import com.hyundai.hpass.socialLogIn.SocialLoginActivity
+import com.hyundai.hpass.socialLogIn.SocialLoginViewModel
+import com.hyundai.hpass.subscription.AddSubscriptionActivity
 import com.hyundai.hpass.subscription.SubscriptionMainActivity
 
 class HamburgerMenuActivity : AppCompatActivity() {
 
+    private lateinit var viewModel: SocialLoginViewModel
     private lateinit var binding: HamburgerMenuActivityBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = HamburgerMenuActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        viewModel = ViewModelProvider(this)[SocialLoginViewModel::class.java]
         configureUI()
         configureEvent()
+        bind()
     }
 
     private fun configureUI() {
@@ -37,21 +45,41 @@ class HamburgerMenuActivity : AppCompatActivity() {
                 val intent = Intent(this, MyPageMainActivity::class.java)
                 startActivity(intent)
             } else {
-                val intent = Intent(this, SubscriptionMainActivity::class.java)
-                startActivity(intent)
+                viewModel.authenticateNaver(this@HamburgerMenuActivity)
             }
         }
 
         binding.menuText.setOnClickListener {
-            if (MyApplication.preferences.getString("loginPass") == "false") {
+            Log.d("HamburgerMenuActivity: 로그인 여부",MyApplication.preferences.getString("loginPass"))
+            Log.d("HamburgerMenuActivity: 구독 여부",MyApplication.preferences.getString(BuildConfig.PREF_KEY_SUBS))
+            if (MyApplication.preferences.getString("loginPass") != "true") {
                 Toast.makeText(this, "로그인이 필요합니다.", Toast.LENGTH_SHORT).show()
-            } else if (false == false) {
+            } else if (MyApplication.preferences.getString(BuildConfig.PREF_KEY_SUBS) == BuildConfig.PREF_VALUE_TRUE) {
                 // 구독자일 경우
                 val intent = Intent(this, SubscriptionMainActivity::class.java)
                 startActivity(intent)
             } else {
                 // 구독 가입 페이지로 이동
+                val intent = Intent(this, AddSubscriptionActivity::class.java)
+                startActivity(intent)
             }
         }
+    }
+    private fun bind() {
+        viewModel.getLoginSuccess().observe(this) { success ->
+            if (success) {
+                goToMain()
+            }
+        }
+        viewModel.errorMessage.observe(this) { message ->
+            Log.d("NaverLogin", "Error: $message")
+        }
+    }
+    private fun goToMain() {
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+        }
+        startActivity(intent)
+        finish()
     }
 }
